127a3ff80116f5893d2cc1dc3eaa745ee7525a0b
husen <husen@zdns.cn>
2022-09-15 20:12:10 +0800

    utilStringToArray 兼容性扩展， 支持输入array

--- src/utils/utilStringToArray.ts
+++ src/utils/utilStringToArray.ts
@@ -1,15 +1,21 @@
 /**
  * 字符串 转 数组
  * String to Array
+ * @param[str] 需要拆分的数据
+ * @param[separator] 拆分标识符，默认 ,
  * */
 const utilStringToArray = (str: any, separator?: string) => {
+  separator = separator || ','
+
+  if (Array.isArray(str)) {
+    return str
+  }
+
   if (typeof str === 'string') {
     str = str.trim();
     return str.split(separator || /\s+/);
   }
-  if (Array.isArray(str)) {
-    return str
-  }
+
   return [];
 };
 
