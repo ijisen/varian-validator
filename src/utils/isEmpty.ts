4ab53f2c1e7c9912e1127f5e97d5679c490ea4fb
husen <husen@zdns.cn>
2022-09-22 17:25:19 +0800

    isEmpty 入参优化

--- src/utils/isEmpty.ts
+++ src/utils/isEmpty.ts
@@ -25,7 +25,7 @@ export const isEmptyStr = (str: any, ignoreSpace: boolean): boolean => {
  * @names：判断数组是否为空数据
  * @params[data] Array
  * */
-export const isEmptyArray = (data = []): boolean => {
+export const isEmptyArray = (data: any): boolean => {
   return !Array.isArray(data) || !data.length
 };
 
