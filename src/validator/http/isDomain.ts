5329cc9c61532fca00f5cb9e289cde581d82bf33
husen <husen@zdns.cn>
2022-12-22 16:46:18 +0800

    域名合法性校验逻辑添加国际化参数

--- src/validator/http/isDomain.ts
+++ src/validator/http/isDomain.ts
@@ -1,20 +1,20 @@
 import isFQDN from './isFQDN'
-import { isFQDNRes } from "./typings.d";
+import {isFQDNRes} from "./typings.d";
 
 /**
  * 域名合法性校验
  * */
-const isDomain = (str: string): isFQDNRes => {
-  return isFQDN(str, {
-    require_tld: true,
-    // 是否允许包含下划线
-    allow_underscores: true,
-    // 是否允许 . 号结尾
-    allow_trailing_dot: false,
-    // 是否允许数字TLD号结尾
-    allow_numeric_tld: false,
-    // 是否运通配符 *
-    allow_wildcard: false
-  });
+const isDomain = (str: string, lang?: string): isFQDNRes => {
+    return isFQDN(str, {
+        require_tld: true,
+        // 是否允许包含下划线
+        allow_underscores: true,
+        // 是否允许 . 号结尾
+        allow_trailing_dot: false,
+        // 是否允许数字TLD号结尾
+        allow_numeric_tld: false,
+        // 是否运通配符 *
+        allow_wildcard: false
+    }, lang);
 };
 export default isDomain
