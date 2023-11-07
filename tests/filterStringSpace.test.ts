819b74832d76e2d57e0d7b0a307307b5a0fe2a8a
husen <husen@zdns.cn>
2023-11-07 11:12:54 +0800

    test 测试用例编写

--- tests/filterStringSpace.test.ts
+++ tests/filterStringSpace.test.ts
@@ -3,7 +3,10 @@ import { filterStringSpace } from "../lib";
 test('filterStringSpace test', () => {
   expect(filterStringSpace(null)).toStrictEqual('');
   expect(filterStringSpace(undefined)).toStrictEqual('');
+  expect(filterStringSpace(5)).toStrictEqual('');
   expect(filterStringSpace('')).toStrictEqual('');
   expect(filterStringSpace('          ')).toStrictEqual('');
-  expect(filterStringSpace('  a      a  ')).toStrictEqual(false);
+  expect(filterStringSpace('  a      a  ', true)).toStrictEqual('aa');
+  expect(filterStringSpace('  a      a  ')).toStrictEqual('a      a');
+  expect(filterStringSpace('  a    ')).toStrictEqual('a');
 })
