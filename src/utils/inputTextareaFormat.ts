894a1998a100d9157b3678823a4e99b0733f7699
husen <husen@zdns.cn>
2024-08-15 16:55:45 +0800

    feat: inputTextareaFormat 字符转小写 添加判断参数， 默认转小写

--- src/utils/inputTextareaFormat.ts
+++ src/utils/inputTextareaFormat.ts
@@ -3,6 +3,7 @@ import { specialSymbolToComma } from './specialSymbolToComma';
 /**
  * @names：textarea 输入内容格式化
  * @params[str] string
+ * @params[needToLowerCase] boolean 是否需要转小写
  * @return [] Array
  * @description:
  * 1、去掉首位空格
@@ -11,14 +12,16 @@ import { specialSymbolToComma } from './specialSymbolToComma';
  * 4、specialSymbolToComma 转 英文,
  * 5、数组去重
  * */
-export const inputTextareaFormat = (str: any): any[] => {
+export const inputTextareaFormat = (str: any, needToLowerCase?: boolean): any[] => {
   if (typeof str !== 'string') {
     return [];
   }
   // 去除首尾空格
   str = str.trim();
   // 大写转小写
-  str = str.toLowerCase();
+  if (needToLowerCase !== false) {
+    str = str.toLowerCase();
+  }
   // 去除多余的空格
   str = str.replace(/\s+|\n+/g, ' ');
   // 特殊符号转 ,
